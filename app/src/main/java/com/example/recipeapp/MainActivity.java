87b92cc327885cafff5d06ee05c1ca1e7efd685e
gpallavi22 <gpallavi803@gmail.com>
2022-05-06 01:27:20 +0530

    RecipeApp

---
+++ app/src/main/java/com/example/recipeapp/MainActivity.java
@@ -0,0 +1,92 @@
+package com.example.recipeapp;
+
+import android.app.ProgressDialog;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.recipeapp.Adapters.RandomRecipeAdapter;
+import com.example.recipeapp.Listeners.RandomRecipeResponseListener;
+import com.example.recipeapp.Models.RandomRecipeApiResponse;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+
+    ProgressDialog dialog;
+    RequestManager manager;
+    RandomRecipeAdapter randomRecipeAdapter;
+    RecyclerView recyclerView;
+    Spinner spinner;
+    List<String> tags = new ArrayList<>();
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        dialog = new ProgressDialog((this));
+        dialog.setTitle("Loading...");
+
+        spinner = findViewById(R.id.spinner_tags);
+        ArrayAdapter arrayAdapter = ArrayAdapter.createFromResource(
+                this ,
+                R.array.tags ,
+                R.layout.spinner_text
+        );
+        arrayAdapter.setDropDownViewResource(R.layout.spinner_inner_text);
+        spinner.setAdapter(arrayAdapter);
+        spinner.setOnItemSelectedListener(spinnerSelectedListener);
+
+        manager = new RequestManager(this);
+//        manager.getRandomRecipes(randomRecipeResponseListener);
+//        dialog.show();
+
+
+    }
+    private  final RandomRecipeResponseListener randomRecipeResponseListener = new RandomRecipeResponseListener() {
+        @Override
+        public void didFetch(RandomRecipeApiResponse response, String message) {
+            dialog.dismiss();
+            recyclerView = findViewById(R.id.recycler_random);
+            recyclerView.setHasFixedSize(true);
+            recyclerView.setLayoutManager(new GridLayoutManager(MainActivity.this,1));
+            randomRecipeAdapter = new RandomRecipeAdapter(MainActivity.this ,response.recipes);
+            recyclerView.setAdapter(randomRecipeAdapter);
+
+        }
+
+        @Override
+        public void didError(String message) {
+
+            Toast.makeText(MainActivity.this, "message", Toast.LENGTH_SHORT).show();
+        }
+    };
+
+    private final AdapterView.OnItemSelectedListener spinnerSelectedListener = new AdapterView.OnItemSelectedListener() {
+        @Override
+        public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+            tags.clear();
+            tags.add(adapterView.getSelectedItem().toString());
+            manager.getRandomRecipes(randomRecipeResponseListener, tags);
+            dialog.show();
+
+
+        }
+
+        @Override
+        public void onNothingSelected(AdapterView<?> adapterView) {
+
+
+        }
+    };
+}
